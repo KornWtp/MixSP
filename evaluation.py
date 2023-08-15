01b91daad34a635aa4a9ebf4e213f9205a67f03a
KornWtp <elecwpr@gmail.com>
2023-08-15 14:14:48 +0700

    Update evaluation

--- evaluation.py
+++ evaluation.py
@@ -1,4 +1,3 @@
-
 import sys
 import io, os
 import numpy as np
@@ -8,17 +7,11 @@ from prettytable import PrettyTable
 import torch
 import transformers
 from sentence_transformers.moe_encoder import MixtureOfExpertsEncoder
+from sentence_transformers.moe_encoder.evaluation import MoECorrelationEvaluator, MoECorrelationEvaluatorAUC
+from data import load_data
 
 # Set up logger
 logging.basicConfig(format='%(asctime)s : %(message)s', level=logging.DEBUG)
-
-# Set PATHs
-PATH_TO_SENTEVAL = './SentEval'
-PATH_TO_DATA = './SentEval/data'
-
-# Import SentEval
-sys.path.insert(0, PATH_TO_SENTEVAL)
-import senteval
 
 def print_table(task_names, scores):
     tb = PrettyTable()
@@ -26,123 +19,68 @@ def print_table(task_names, scores):
     tb.add_row(scores)
     print(tb)
 
-def prepare(params, samples):
-    return
-
-def batcher(params, batch):
-    batch1 = [' '.join(sent) if sent != [] else '.' for sent in batch[0]]
-    batch2 = [' '.join(sent) if sent != [] else '.' for sent in batch[1]]
-    batch = [list(pair) for pair in zip(batch1, batch2)]
-    pred_scores = params['encoder'](batch, convert_to_numpy=True, show_progress_bar=False)
-    return pred_scores
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_name_or_path", type=str, 
             help="Transformers' model name or path")
-    parser.add_argument("--mode", type=str, 
-            choices=['dev', 'test', 'fasttest'],
-            default='test', 
-            help="What evaluation mode to use (dev: fast mode, dev results; test: full mode, test results); fasttest: fast mode, test results")
     parser.add_argument("--task_set", type=str, 
-            choices=['sts', 'transfer', 'full', 'na'],
-            default='sts',
+            choices=['sts_sickr', 'domain_transfer', 'binary_classification', 'na'],
+            default='sts_sickr',
             help="What set of tasks to evaluate on. If not 'na', this will override '--tasks'")
-    parser.add_argument("--tasks", type=str, nargs='+', 
-            default=['STS12', 'STS13', 'STS14', 'STS15', 'STS16',
-                     'MR', 'CR', 'MPQA', 'SUBJ', 'SST2', 'TREC', 'MRPC',
-                     'SICKRelatedness', 'STSBenchmark'], 
-            help="Tasks to evaluate on. If '--task_set' is specified, this will be overridden")
-    
+ 
     args = parser.parse_args()
+    print(args)
     
+    ### read datasets
+    if args.task_set == "sts_sickr":
+        all_pairs, all_test, dev_samples = load_data(args.task_set)
+    elif args.task_set == "domain_transfer":
+        all_pairs = []
+        all_test = []
+        dev_samples = []
+        for name in ["qqp", "qnli", "mrpc"]:
+            pairs, test, dev = load_data(name)
+            all_pairs.append(pairs)
+            all_test.append(test)
+            dev_samples.append(dev)
+
     # Load moe sentence transformers' model checkpoint
     model = MixtureOfExpertsEncoder.from_pretrained(args.model_name_or_path)
+
+    all_sts_name = {"2012": "STS12",
+                    "2013": "STS13",
+                    "2014": "STS14",
+                    "2015": "STS15",
+                    "2016": "STS16",
+                    "stsb": "STSBenchmark",
+                    "sickr": "SICKRelatedness"}
     
-    # Set up the tasks
-    if args.task_set == 'sts':
-        args.tasks = ['STS12', 'STS13', 'STS14', 'STS15', 'STS16', 'STSBenchmark', 'SICKRelatedness']
-    elif args.task_set == 'transfer':
-        args.tasks = ['MR', 'CR', 'MPQA', 'SUBJ', 'SST2', 'TREC', 'MRPC']
-    elif args.task_set == 'full':
-        args.tasks = ['STS12', 'STS13', 'STS14', 'STS15', 'STS16', 'STSBenchmark', 'SICKRelatedness']
-        args.tasks += ['MR', 'CR', 'MPQA', 'SUBJ', 'SST2', 'TREC', 'MRPC']
-
-    # Set params for SentEval
-    if args.mode == 'dev' or args.mode == 'fasttest':
-        # Fast mode
-        params = {'task_path': PATH_TO_DATA, 'usepytorch': True, 'kfold': 5}
-        params['classifier'] = {'nhid': 0, 'optim': 'rmsprop', 'batch_size': 128,
-                                         'tenacity': 3, 'epoch_size': 2}
-    elif args.mode == 'test':
-        # Full mode
-        params = {'task_path': PATH_TO_DATA, 'usepytorch': True, 'kfold': 10}
-        params['classifier'] = {'nhid': 0, 'optim': 'adam', 'batch_size': 64,
-                                         'tenacity': 5, 'epoch_size': 4}
-    else:
-        raise NotImplementedError
-
-    results = {}
-
-    for task in args.tasks:
-        params = {'task_path': 'SentEval/data', 'usepytorch': True, 'kfold': 10}
-        params['encoder'] = model.predict
-        se = senteval.engine.SE(params, batcher, prepare)
-        result = se.eval(task)
-        results[task] = result
-    
-    # Print evaluation results
-    if args.mode == 'dev':
-        print("------ %s ------" % (args.mode))
-
-        task_names = []
-        scores = []
-        for task in ['STSBenchmark', 'SICKRelatedness']:
-            task_names.append(task)
-            if task in results:
-                scores.append("%.2f" % (results[task]['dev']['spearman'][0] * 100))
-            else:
-                scores.append("0.00")
-        print_table(task_names, scores)
-
-        task_names = []
-        scores = []
-        for task in ['MR', 'CR', 'SUBJ', 'MPQA', 'SST2', 'TREC', 'MRPC']:
-            task_names.append(task)
-            if task in results:
-                scores.append("%.2f" % (results[task]['devacc']))    
-            else:
-                scores.append("0.00")
+    task_names = []
+    scores = []
+    if args.task_set == "sts_sickr":
+        logging.info ("########### STS Results ##########")
+        for name, data in all_test.items():
+            test_evaluator = MoECorrelationEvaluator.from_input_examples(data, name=name)
+            task_names.append(all_sts_name[name])
+            scores.append("%.2f" % (test_evaluator(model) * 100))
         task_names.append("Avg.")
         scores.append("%.2f" % (sum([float(score) for score in scores]) / len(scores)))
         print_table(task_names, scores)
 
-    elif args.mode == 'test' or args.mode == 'fasttest':
-        print("------ %s ------" % (args.mode))
 
-        task_names = []
-        scores = []
-        for task in ['STS12', 'STS13', 'STS14', 'STS15', 'STS16', 'STSBenchmark', 'SICKRelatedness']:
-            task_names.append(task)
-            if task in results:
-                if task in ['STS12', 'STS13', 'STS14', 'STS15', 'STS16']:
-                    scores.append("%.2f" % (results[task]['all']['spearman']['all'] * 100))
-                else:
-                    scores.append("%.2f" % (results[task]['test']['spearman'].correlation * 100))
-            else:
-                scores.append("0.00")
-        task_names.append("Avg.")
-        scores.append("%.2f" % (sum([float(score) for score in scores]) / len(scores)))
-        print_table(task_names, scores)
+    transfer_name = {"qqp": "QQP",
+                    "qnli": "QNLI",
+                    "mrpc": "MRPC"}
 
-        task_names = []
-        scores = []
-        for task in ['MR', 'CR', 'SUBJ', 'MPQA', 'SST2', 'TREC', 'MRPC']:
-            task_names.append(task)
-            if task in results:
-                scores.append("%.2f" % (results[task]['acc']))    
-            else:
-                scores.append("0.00")
+    task_names = []
+    scores = []
+    if args.task_set == "domain_transfer":
+        logging.info ("########### Domain Transfer Results ###########")
+        for test in all_test:
+            for name, data in test.items():
+                test_evaluator = MoECorrelationEvaluatorAUC.from_input_examples(data, name=name)
+                task_names.append(transfer_name[name])
+                scores.append("%.2f" % (test_evaluator(model) * 100))
         task_names.append("Avg.")
         scores.append("%.2f" % (sum([float(score) for score in scores]) / len(scores)))
         print_table(task_names, scores)
